c131e2cb3ef6b546a5aa35fbf0feadc7f79c694a
Gert Findel <gert@acid.cl>
2011-11-12 16:40:29 -0300

    db integration

--- acidjobsbot.rb
+++ acidjobsbot.rb
@@ -1,5 +1,5 @@
 require File.join(File.dirname(__FILE__),"acidjobsbot/config")
 require File.join(File.dirname(__FILE__),"acidjobsbot/bot")
 
-acid_bot = AcidJobsBot::AcidTweetBot.new
+acid_bot = AcidJobsBot::Bot.new
 acid_bot.run
--- acidjobsbot/bot.rb
+++ acidjobsbot/bot.rb
@@ -1,14 +1,24 @@
+require File.join(File.dirname(__FILE__),"../db/migrate/create_job_offers")
+
 module AcidJobsBot
   
-  class AcidTweetBot 
+  class Bot 
     
     def initialize
       @@config ||= AcidJobsBot::Config.new
     end
     
     def run
-      puts "Hello World Marica"
-      puts @@config.consumer_key
+      puts "Running AcidTweetBot"
+      begin
+         offer = AcidJobsBot::JobOffer.find(:first)
+         tweet_offer(offer)
+        
+         # create the table
+         CreateJobOffers.up
+         puts "No JobOffers in the database"
+        AcidJobsBot::JobOffer.create({:description => 'test status'})
+      end 
     end
 
   end
